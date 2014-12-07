54c0dcf52a9b3290c3c08ae80a278575e388a0ca
robludwig <robludwig@gmail.com>
2014-12-07 13:05:33 -0500

    Add naive evaluation

--- Main/Main.py
+++ Main/Main.py
@@ -5,8 +5,12 @@ Created on Dec 6, 2014
 '''
 
 from Board import Board
+from Evalution.EvaluateBoard import evaluate
 
 if __name__ == '__main__':
     board = Board.Board()
     board.reset_to_starting_positiion()
     print(board)
+    print(board.get_white_pieces())
+    print(sum([piece.get_base_value() for piece in board.get_black_pieces()]))
+    print(evaluate(board))
